d0885ba4c3412d8efd1b4bc077fa5abecb9b7264
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2022-04-28 15:47:24 -0700

    Use common.util.StringUtils instead of core.instrument.util

--- micrometer-core/src/main/java/io/micrometer/core/instrument/binder/db/JooqExecuteListener.java
+++ micrometer-core/src/main/java/io/micrometer/core/instrument/binder/db/JooqExecuteListener.java
@@ -15,10 +15,10 @@
  */
 package io.micrometer.core.instrument.binder.db;
 
+import io.micrometer.common.util.StringUtils;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
 import io.micrometer.core.instrument.Timer;
-import io.micrometer.core.instrument.util.StringUtils;
 import java.util.HashMap;
 import java.util.Map;
 import org.jooq.ExecuteContext;
